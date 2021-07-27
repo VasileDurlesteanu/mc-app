d9d9d7e6fd7fbabc1c71233f86f2628e630c2312
VasileDurlesteanu <vvdurlesteanu@gmail.com git config --global user.name VasileDurlesteanugit config --global user.email vvdurlesteanu@gmail.com>
2021-07-27 14:10:12 +0300

    angular-devextreme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mc-app';
+
+  menuItems = [{
+    name: 'Video Players',
+    items: [
+        { name: 'HD Video Player', price: 220 },
+        { name: 'SuperHD Video Player', price: 270 }
+    ]
+}, {
+    name: 'Televisions',
+    items: [
+        { name: 'SuperLCD 42', price: 1200 },
+        { name: 'SuperLED 42', price: 1450 },
+        { name: 'SuperLED 50', price: 1600 }
+    ]
+}, {
+    name: 'Monitors',
+    items: [{
+        name: '19"',
+        items: [
+            { name: 'DesktopLCD 19', price: 160 }
+        ]
+    }, {
+        name: '21"',
+        items: [
+            { name: 'DesktopLCD 21', price: 170 },
+            { name: 'DesktopLED 21', price: 175 }
+        ]
+    }]
+}];
+
 }
